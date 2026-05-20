c9c76db38f82632cb6eb3d0652893e415b8f8bd2
Shahriar-Hossain-Alvi <shahriarhossainalvi@gmail.com>
2026-05-20 23:04:11 +0600

    added and configured env variables

---
+++ app/core/config.py
@@ -0,0 +1,52 @@
+from typing import Any
+from pydantic import field_validator, SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+# load env variables from .env and make it available to the app with the name settings
+class Settings(BaseSettings):
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: SecretStr
+    POSTGRES_DB: str
+
+    DATABASE_URL: str
+    SYNC_DATABASE_URL: str
+
+    SECRET_KEY: SecretStr
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    REFRESH_TOKEN_EXPIRE_MINUTES: int = 604800
+
+    APP_ENV: str
+
+    # Splits the cors origins string and converts into a list
+    CORS_ORIGINS: Any = []
+
+    @field_validator("CORS_ORIGINS", mode="before")
+    @classmethod
+    def assemble_cors_origins(cls, value: Any) -> list[str]:
+        # Handle string input from env
+        if isinstance(value, str):
+            # if its a string, split by comma
+            if not value.startswith("["):
+                # Handles "url1, url2" -> ["url1", "url2"]
+                return [item.strip() for item in value.split(",") if item.strip()]
+
+            import json
+            try:
+                return json.loads(value)
+            except:
+                return []
+
+        if isinstance(value, (list, tuple)):
+            return list(value)
+
+        return []
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore"  # it ignores the env variables that are not in the pydantic model
+    )
+
+
+settings = Settings()  # type: ignore
--- app/main.py
+++ app/main.py
@@ -1,8 +1,33 @@
 from fastapi import FastAPI
+from app.core.config import settings
 
-app = FastAPI()
+# check if in production or development
+ENV = settings.APP_ENV or "development"
+
+# disable swagger and redoc in production
+if ENV == "production":
+    app = FastAPI(
+        docs_url=None,
+        redoc_url=None,
+        openapi_url=None
+    )
+else:
+    app = FastAPI(
+        # send HttpOnly Cookies to Swagger UI
+        swagger_ui_parameters={"withCredentials": True}
+    )
 
 
+# add this to wake up the server
 @app.get("/")
+@app.head("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"status": "awake"}
+
+
+# crete a router to check db health using select 1
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
